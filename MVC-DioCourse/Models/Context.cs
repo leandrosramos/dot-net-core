617376cba199e1460c80e48cdf606ac104072888
leandrosramos <leandro.sramos@outlook.com>
2020-09-14 01:51:17 -0300

    Code changes applieds in 14/setember/2020.

---
+++ MVC-DioCourse/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_DioCourse.Models
+{
+    public class Context : DbContext
+    { 
+        public DbSet<Category> Categories { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            //Configuring the connection string for Sql Server. 
+            //@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True"
+            optionsBuilder.UseSqlServer(@"Server=ARSESP_089;Database=MVC_DioCourse;Integrated Security=True");
+        }
+    }
+}
--- MVC-DioCourse/Startup.cs
+++ MVC-DioCourse/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVC_DioCourse.Models;
 
 namespace MVC_DioCourse
 {
@@ -24,6 +25,7 @@ namespace MVC_DioCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
